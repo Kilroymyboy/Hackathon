038bcbc96e02d2f1dcefb05626b4bd37697ca9cc
Aoliv <abigailmoliver@gmail.com>
2017-02-22 02:28:39 -0700

    init gui

---
+++ Assets/Scripts/buttonManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//If you wanna change scenes for different levels
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClearClick()
+    {
+
+    }
+
+    public void onPlayClick()
+    {
+
+    }
+
+    public void onTestClick()
+    {
+
+    }
+
+    public void onIfClick()
+    {
+
+    }
+
+    public void onElseClick()
+    {
+
+    }
+
+    public void onWhileClick()
+    {
+
+    }
+
+    public void onStairsClick()
+    {
+
+    }
+
+    public void onRockClick()
+    {
+
+    }
+
+    public void onTreeClick()
+    {
+
+    }
+
+    public void onPushClick()
+    {
+
+    }
+
+    public void onChopClick()
+    {
+
+    }
+
+    public void onClimbClick()
+    {
+
+    }
+
+
+    public void onNextLevelClick()
+    {
+        //If you wanna change scenes for different levels
+        SceneManager.LoadScene("testScene", LoadSceneMode.Single);
+    }
+}
